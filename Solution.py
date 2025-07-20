134831b10e83a921a0871594e7daea7af7674b55
Baha Aydogan <bahaydogann@gmail.com>
2025-07-20 15:59:21 +0300

    leetcode 541

--- Solution.py
+++ Solution.py
@@ -15,3 +15,18 @@ class Solution(object):
                 prev = f
 
         return result
+
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = list(s)
+        for i in range(0, len(s), 2 * k):
+            s[i:i + k] = reversed(s[i:i + k])
+        return ''.join(s)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseStr("abcdefg", 2))
